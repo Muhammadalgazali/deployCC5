0061cbab97437f3c0033f03d8b11a3bce3b4c811
Muhammadalgazali <58090838+Muhammadalgazali@users.noreply.github.com>
2023-03-31 14:04:10 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>About C173DKX4816</title>
+
+    <!-- CSS Utama -->
+    <link rel="stylesheet" href="app.css" />
+
+    <!-- Font Awesome 5.15.4 -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+      integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+  </head>
+  <body>
+
+    <header class="navbar-container">
+      <div class="logo">
+        <h2>
+          <strong>My Personal Information</strong>
+        </h2>
+      </div>
+
+      <nav class="nav-list">
+        <ul>
+          <li><p>Social Media :</p></li>
+          <li><a href="https://www.instagram.com/all.ghazaliiii/">
+            <img src="instagram.png" alt="Instagram"></a>
+          </li>
+          <li><a href="https://www.linkedin.com/in/muhammad-algazali-9b1476253/">
+            <img src="linkedin.png" alt="Linkedlin"></a>
+          </li>
+          <li><a href="https://www.facebook.com/muhammadalgazali019/">
+            <img src="facebook.png" alt="Facebook"></a>
+          </li>
+          <li><a href="https://www.cloudskillsboost.google/public_profiles/3d819acf-41c9-456d-851f-60bc74876051">
+            <img src="cloud.png" alt="SkillBoost"></a>
+          </li>
+          <li><a href="https://github.com/Muhammadalgazali">
+            <img src="github.png" alt="Github"></a>
+          </li>
+        </ul>
+        </ul>
+      </nav>
+    </header>
+
+    <!--Main Content-->
+    <main>
+      <!--#banner-->
+      <div class="content" style="border: 0px">
+        
+        <!-- namaku -->
+        <div class="content-description">
+          <p>Cloud Computing - 02</p>
+          <h1 class="title">Muhammad Algazali</h1>
+          <p>a.k.a 
+          <strong style="font-size: 30px" id="text" onclick="rubahKata()">C173DKX4816</strong>
+        </p>
+        </div>
+        <!-- fotoku -->
+        <div class="content-image">
+          <span align="center">
+            <img src="foto-profil.jpg" alt="Cyber Security" />
+          </span>
+        </div>
+      </div>
+      <hr />
+
+      <aside>
+        <div class="content" style="margin-top: 10px">
+          
+          <!-- my Stories -->
+          <div class="content-description">
+            <h5 class="title2" style="margin-bottom: 10px; margin-top: 0px;">My Story</h5>
+            <div style="width: 100%; padding-right: 40px; font-size: 15px">
+              As a student of Information Engineering at Halu Oleo University,
+              my primary interest lies in cybersecurity. A few months back, I
+              delved deeper into the subject matter by studying ways to secure
+              sensitive data on websites. Being an IT student, I am highly
+              motivated to constantly augment my knowledge and refine my skills,
+              particularly in safeguarding website data, especially in the
+              context of cloud computing. As part of this journey, I have
+              enrolled in Google's Bangkit Academy Cloud Computing Program to
+              gain me a more comprehensive understanding of transferring data
+            </div>
+          </div>
+
+          <!--Keterangan Biodata-->
+          <div class="content-description" >
+            <h5 class="title2" style="margin-bottom: 0px; margin-top: 20px;">Author Biodata</h5>
+            <div style="padding-right: 40px;">
+            <table>
+              <tr>
+                <td style="width:30%"></td>
+                <td></td>
+                <td style="width:68%"></td>
+              </tr>
+              <tr>
+                <td><b>Full Name</b></td>
+                <td>:</td>
+                <td>Muhammad Algazali</td>
+              </tr>
+              <tr>
+                <td><b>Major</b></td>
+                <td>:</td>
+                <td>Teknik Informatika</td>
+              </tr>
+              <tr>
+                <td><b>University</b></td>
+                <td>:</td>
+                <td>Universitas Halu Oleo</td>
+              </tr>
+              <tr>
+                <td><b>Phone Number</b></td>
+                <td>:</td>
+                <td>+62 853-4162-0297</td>
+              </tr>
+              <tr>
+                <td><b>Personal Email</b></td>
+                <td>:</td>
+                <td style="word-wrap: break-word;">muhammadalgazali999@gmail.com</td>
+              </tr>
+              <tr>
+                <td><b>Bangkit Email</b></td>
+                <td>:</td>
+                <td style="word-wrap: break-word;">c173dkx4816@bangkit.academy</td>
+              </tr>
+            </table>
+          </div>
+
+          </div>
+
+        </div>
+      </aside>
+
+    </main>
+  </body>
+  <script src="script.js"></script>
+</html>
